42b4bbf6b7f659ef01975d32014dd0f663d303a2
Sai Rohit <sairohit@Sais-MacBook-Pro.local>
2023-06-19 16:17:22 +0530

    gh-changes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ import Conductor from './pages/Conductor'
 const App = () => {
   return (
     <div>
-        <BrowserRouter>
+        <BrowserRouter basename = "/mainsite">
             <Routes>
-                <Route path = "/" element = {<Home/>} />
+                <Route path = "/mainsite" element = {<Home/>} />
                 <Route path = "/Fapster" element = {<Fapster/>} />
                 <Route path = "/Wingman" element = {<Wingman/>} />
                 <Route path = "/Northstage" element = {<Northstage/>} />
